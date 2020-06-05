0f28d98f74215ed5d1600674e38ab250a4bd314a
Hector Diaz <3kthor3adward@gmail.com>
2020-06-05 07:01:04 -0500

    port list index

--- webpack-app/src/components/Home.vue
+++ webpack-app/src/components/Home.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+  <div>
+    <h1>Home</h1>
+    <lists v-bind:lists="lists"></lists>
+  </div>
 </template>
 
 <script>
+const axios = require('axios');
+import Lists from "./Lists.vue";
+
 export default {
-    name: 'Home'
-}
+  name: "Home",
+  components: {
+    Lists
+  },
+  data: function() {
+    return {
+      apiUrl: "http://localhost:8080",
+      list: {
+        title: ""
+      },
+      lists: []
+    };
+  },
+  methods: {
+    refreshList: function() {
+      axios
+        .get(this.apiUrl + "/lists")
+        .then(json => (this.lists = json.data.data));
+    }
+  },
+  mounted: function() {
+    this.refreshList();
+  }
+};
 </script>
---
+++ webpack-app/src/components/Lists.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="list in lists" v-bind:key="list.id">
+      <a v-bind:href="'/app/list_detail.html?id=' + list.id">{{list.title}}</a>
+      <button class="fas fa-trash" v-on:click="$emit('remove-list', list.id)"></button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "lists",
+  props: ['lists']
+};
+</script>
