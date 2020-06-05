0959974ea3b47a06eb5c08e608f756c99c403154
Hector Diaz <3kthor3adward@gmail.com>
2020-06-05 12:07:13 -0500

    add font-awesome vue component

--- webpack-app/src/app.js
+++ webpack-app/src/app.js
@@ -1,6 +1,26 @@
 import Vue from 'vue';
 import Main from './components/Main.vue';
 import router from './router';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faThumbtack}  from '@fortawesome/free-solid-svg-icons';
+import {faCheck} from '@fortawesome/free-solid-svg-icons';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons';
+import {faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faComments} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+library.add(
+    faTrash,
+    faThumbtack,
+    faCheck,
+    faSpinner,
+    faPlusCircle,
+    faEdit,
+    faComments
+    );
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
     render: (h) => h(Main),
