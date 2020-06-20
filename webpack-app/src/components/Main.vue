ce04d10df85ee8daf53ca25f90614a097b184bde
Hector Diaz <3kthor3adward@gmail.com>
2020-06-20 17:18:32 -0500

    init todo api

--- webpack-app/src/components/Main.vue
+++ webpack-app/src/components/Main.vue
@@ -3,11 +3,28 @@
     <div>
         <router-link to="/home">home</router-link>
         <router-view></router-view>
+
+        <button v-if="!logedIn" v-on:click="login">login</button>
+        <button v-else v-on:click="logout">logout</button>
     </div>
 </template>
 
 <script>
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    props: ['logedIn'],
+    methods: {
+        login: function(){
+            this.$auth.login()
+                .then(() => console.log("login ok"))
+                .catch(() => this.errors.push({}));
+        },
+        logout: function(){
+            this.$auth.logout()
+                .then(() => console.log("logout ok"))
+                .catch(() => this.errors.push({}));
+        }
+    }
 }
 </script>
